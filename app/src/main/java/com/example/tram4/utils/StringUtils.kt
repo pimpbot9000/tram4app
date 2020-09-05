e9fa347150b6fc9ce42f5a0dbad10554f20ee0c5
Tuomas Välimäki <t.valimaki@gmail.com>
2020-09-05 20:46:34 +0300

    Remove some commented out code

--- app/src/main/java/com/example/tram4/utils/StringUtils.kt
+++ app/src/main/java/com/example/tram4/utils/StringUtils.kt
@@ -5,11 +5,7 @@ import android.content.Context
 object StringUtils{
     fun parseStringArrayAsMap(ctx: Context, stringArrayResourceId: Int): Map<String, String> {
         val stringArray = ctx.resources.getStringArray(stringArrayResourceId)
-        /*val outputArray = SparseArray<String>(stringArray.size)
-        for (entry in stringArray) {
-            val splitResult = entry.split("\\|".toRegex(), 2).toTypedArray()
-            outputArray.put(splitResult[0], splitResult[1])
-        }*/
+
         return stringArray.map{
             val split = it.split("\\|".toRegex(), 2).toTypedArray()
             split[0] to split[1]
